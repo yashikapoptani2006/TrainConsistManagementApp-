eb0e14b403e9916f120d52d9d98510764f1fed49
Yashika Poptani <poptaniyashika6@gmail.com>
2026-04-07 15:41:43 +0530

    UC5: Maintain ordered and unique train formation using LinkedHashSet

--- trainconsistapp/src/TrainApp.java
+++ trainconsistapp/src/TrainApp.java
@@ -1,19 +1,15 @@
 package com.train.app;
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 public class TrainApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-        LinkedList<String> trainConsist = new LinkedList<>();
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
-        System.out.println("Initial Train Consist: " + trainConsist);
-        trainConsist.add(2, "Pantry Car");
-        System.out.println("After Adding Pantry Car: " + trainConsist);
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-        System.out.println("Final Train Consist: " + trainConsist);
+        Set<String> trainFormation = new LinkedHashSet<>();
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+        trainFormation.add("Sleeper");
+        System.out.println("Train Formation: " + trainFormation);
     }
 }
